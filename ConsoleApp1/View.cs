55543dbc27e991cc75839d8c128327959e842ebc
lukasz <lukaszbondar10@gmail.com>
2024-10-31 15:46:05 +0100

    dodanie podstawowych podstawowych widokow i nawigacja

--- ConsoleApp1/View.cs
+++ ConsoleApp1/View.cs
@@ -36,38 +36,43 @@ class View
 
     private void generateLayout()
     {
-
+        // Tworzenie panelu widoku produktów
         Panel productView = new Panel(table.Alignment(Justify.Left)).Border(BoxBorder.None);
 
+        // Inicjalizacja ActionPanel
         ActionPanel = new Panel("Hello").Expand();
 
+        // Ustawienie TopBar z nazwą "Navbar"
         TopBar.Update(TopNavBar.GetTable().Width(150).Alignment(Justify.Center).Expand());
+        TopBar.Name = "Navbar"; // Nazwa wymagana, by AppState mogło znaleźć i aktualizować Navbar
 
-        RootLayout.SplitRows(TopBar, BottomView);
+        // Główna struktura RootLayout
+        RootLayout.SplitRows(
+            TopBar, // dodanie TopBar jako Navbar
+            BottomView
+        );
 
+        // Przypisanie nazw i zaktualizowanie zawartości layoutów dla widoków
         ProductLayout.Update(productView);
-
+        ProductLayout.Name = "Content"; // Nazwa wymagana dla widoków, aby AppState mogło aktualizować zawartość
         RightBottomView.SplitRows(ActionLayout, HelpLayout);
-
         BottomView.SplitColumns(ProductLayout, RightBottomView);
 
-
-        HelpLayout.Update(new Help("[bold blue]Use arrows to navigate[/]\n[bold yellow]Press S to search[/]\n[bold red]Press Q to quit[/]").GetPanel().Border(BoxBorder.Rounded));
-
+        // Konfiguracja panelu pomocy i działania
+        HelpLayout.Update(new Help("[bold blue]Use arrows to navigate[/]\n[bold yellow]Press Enter to select[/]").GetPanel().Border(BoxBorder.Rounded));
         ActionLayout.Update(ActionPanel);
 
         TopBar.Size(5);
 
+        // Ukrycie kursora i uruchomienie stanu aplikacji
         var cursor = AnsiConsole.Cursor;
-
         cursor.Hide();
 
+        // Inicjalizacja AppState z odpowiednią konfiguracją RootLayout
         state = new AppState(TopNavBar, RootLayout);
 
+        // Uruchomienie renderowania i nasłuchiwania
         Task.Run(() => state.RenderWindow());
-
-
         state.Listen();
-
     }
 }
